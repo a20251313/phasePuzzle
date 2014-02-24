f28c5e1975f9a5f4b79274e8e40f393405e5459c
a20251313 <a20251313@163.com>
2014-02-24 16:56:08 +0800

    init commit

---
+++ ParseForSqlite/JFSQLManger.h
@@ -0,0 +1,16 @@
+//
+//  JFSQLManger.h
+//  ParseForSqlite
+//
+//  Created by ran on 14-2-24.
+//  Copyright (c) 2014年 com.lelechat.chengyuwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFSQLManger : NSObject
+
+
++(void)insertToSql:(NSString*)strQuestion answer:(NSString*)strAnswer maintype:(int)mainType subType:(int)subType levelType:(int)leveltype  index:(int)index  typeString:(NSString*)typeString isAnswer:(int)isAnswer;
++(int)getMaxIndexFromTablePuzzleTable;
+@end
